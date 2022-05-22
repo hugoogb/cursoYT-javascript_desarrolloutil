f6cf586f1d46c65ec9e81812129edcc96074dbbe
Hugo GB <hugogaben8.02@gmail.com>
2022-05-22 18:51:19 +0200

    started javascript course

---
+++ index.js
@@ -0,0 +1,34 @@
+// const TASA_IVA = 1.21;
+
+// let precioProducto1 = 25;
+// precioProducto1 = precioProducto1 * TASA_IVA;
+// let precio2 = 15;
+// precio2 = precio2 * TASA_IVA;
+
+// const cantidad1 = 2;
+// const cantidad2 = 3;
+
+// const total = precioProducto1 * cantidad1 + precio2 * cantidad2;
+
+// console.log(total);
+
+// const edad = 19;
+
+// if (true) {
+// 	console.log("True: Mi nombre es 'Hugo' y tengo " + edad + " años");
+// } else {
+// 	console.log(`False: Mi nombre es "Hugo" y tengo ${edad} años`);
+// }
+
+// const nulo = null;
+// const indefinido = undefined;
+
+// console.log(typeof []);
+
+let resultado = "2" + 2;
+
+console.log(resultado);
+
+resultado *= 2;
+
+console.log(resultado);
